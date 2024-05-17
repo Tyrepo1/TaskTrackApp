77dd7e7c44548bfc279906e0223e5fa8bef593ca
Tyrepo1 <dimonkey1912@gmail.com>
2024-05-17 17:00:47 +1000

    fix kubernetes deployment

--- TaskTrack/client/src/api/configAPI.js
+++ TaskTrack/client/src/api/configAPI.js
@@ -1,5 +1,5 @@
 
-export const baseurl = process.env.SERVER_URL || 'http://server-service.default.svc.cluster.local:9000';
+export const baseurl = "http://34.129.206.174:9000/";
 export const GET_USERS = "get-users"
 export const GET_OTP = "get-otp"
 export const VERIFY_OTP = "verify-otp"
